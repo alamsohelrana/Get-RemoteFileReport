71ec035252dedb990043c7986b81cc87424a1d02
Sohel Alam <alam.sohel1990@gmail.com>
2022-04-28 22:52:27 +0530

    Corrected spelling of recipients..

--- Get-RemoteFileReport.ps1
+++ Get-RemoteFileReport.ps1
@@ -2,16 +2,16 @@
 #---------------------------------------------------------------------------------
 ###   fill out the variables as per requirement  ###
 $SMTPServerName      = 'Enterprise-SMTPServer-DNS-Name'
-$ReveipientsList     = "alam.sohel1990@gmail.com;anuj.ray@gmail.com"
+$RecipientsList     = "alam.sohel1990@gmail.com;anuj.ray@gmail.com"
 $SenderMailID        = "anuj.ray@gmail.com"
 $RemoteServerPath    = "\\server\REmoteFolderPath"
 $RemoteFolderNames   = "FOLDER1;FOLDER2;FOLDER3"
 $MailBodySignature   = "Sohel Alam"
 #---------------------------------------------------------------------------------
 
-# Uncomment below line if the mails are not being sent..
-# $ReveipientsList     = $ReveipientsList   -split ';' 
-$RemoteFolderNames   = $RemoteFolderNames -split ';'
+# Uncomment below line for multiple recipients..
+$RecipientsList     = $RecipientsList   -split ';' 
+$RemoteFolderNames  = $RemoteFolderNames -split ';'
 
 Function Get-MailBody{
     param([String]$FolderName)
@@ -72,12 +72,6 @@ Function Get-MailBody{
 
     <p class=MsoNormal>Regards,<o:p></o:p></p>
 
-    <p class=MsoNormal>Anuj Ray<o:p></o:p></p>
-
-    </div>
-
-    </body>
-        
 
     <!-- ========================== Signature ==========================. -->
     <p class=MsoNormal><o:p>&nbsp;</o:p></p>
@@ -90,85 +84,23 @@ Function Get-MailBody{
     <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><b><span lang=EN-US
     style='font-size:10.5pt;mso-fareast-font-family:"Times New Roman";mso-fareast-theme-font:
     minor-fareast;color:#1C23BA;mso-ansi-language:EN-US;mso-fareast-language:EN-IN;
-    mso-no-proof:yes'>Anuj Ray</span></b></span><span style='mso-bookmark:_MailAutoSig'><span
+    mso-no-proof:yes'>MailBody_Signature</span></b></span><span style='mso-bookmark:_MailAutoSig'><span
     lang=EN-US style='font-size:10.5pt;font-family:"Times New Roman",serif;
     mso-fareast-font-family:"Times New Roman";mso-fareast-theme-font:minor-fareast;
     mso-ansi-language:EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'><o:p></o:p></span></span></p>
 
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><span lang=EN-US
-    style='font-size:9.0pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:gray;mso-ansi-language:
-    EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'>Sr. Windows Engineer- IT
-    Infra Operations</span></span><span style='mso-bookmark:_MailAutoSig'><span
-    lang=EN-US style='font-size:10.5pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:black;mso-ansi-language:
-    EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'><o:p></o:p></span></span></p>
-
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><span lang=EN-US
-    style='font-size:9.0pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:gray;mso-ansi-language:
-    EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'>&nbsp;</span></span><span
-    style='mso-bookmark:_MailAutoSig'><span lang=EN-US style='font-size:10.5pt;
-    mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
-    mso-fareast-theme-font:minor-fareast;mso-hansi-font-family:Calibri;mso-bidi-font-family:
-    Calibri;color:black;mso-ansi-language:EN-US;mso-fareast-language:EN-IN;
-    mso-no-proof:yes'><o:p></o:p></span></span></p>
-
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><span lang=EN-US
-    style='font-size:9.0pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:gray;mso-ansi-language:
-    EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'>Mobile&nbsp;&nbsp;&nbsp;&nbsp;
-    :&nbsp;&nbsp;+91 9920075684</span></span><span style='mso-bookmark:_MailAutoSig'><span
-    lang=EN-US style='font-size:10.5pt;mso-fareast-font-family:"Times New Roman";
-    mso-fareast-theme-font:minor-fareast;color:black;mso-ansi-language:EN-US;
-    mso-fareast-language:EN-IN;mso-no-proof:yes'><o:p></o:p></span></span></p>
-
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><span lang=EN-US
-    style='font-size:9.0pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:gray;mso-ansi-language:
-    EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'>Zoom Ext:&nbsp;&nbsp;&nbsp;
-    6128259<o:p></o:p></span></span></p>
-
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'></span><a
-    href="mailto:anuj.ray@intusurg.com"><span style='mso-bookmark:_MailAutoSig'><span
-    lang=EN-US style='font-size:9.0pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:#0563C1;
-    mso-ansi-language:EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'>anuj.ray@intusurg.com</span></span><span
-    style='mso-bookmark:_MailAutoSig'></span></a><span style='mso-bookmark:_MailAutoSig'><span
-    lang=EN-US style='font-size:10.5pt;mso-ascii-font-family:Calibri;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;mso-hansi-font-family:
-    Calibri;mso-bidi-font-family:Calibri;color:black;mso-ansi-language:EN-US;
-    mso-fareast-language:EN-IN;mso-no-proof:yes'><o:p></o:p></span></span></p>
-
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><span lang=EN-US
-    style='font-size:10.5pt;mso-fareast-font-family:"Times New Roman";mso-fareast-theme-font:
-    minor-fareast;color:black;mso-ansi-language:EN-US;mso-fareast-language:EN-IN;
-    mso-no-proof:yes'><o:p>&nbsp;</o:p></span></span></p>
-
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><span lang=EN-US
-    style='font-size:10.0pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:#1630F5;
-    mso-ansi-language:EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'>INTUITIVE</span></span><span
-    style='mso-bookmark:_MailAutoSig'><span lang=EN-US style='font-size:10.5pt;
-    mso-fareast-font-family:"Times New Roman";mso-fareast-theme-font:minor-fareast;
-    color:black;mso-ansi-language:EN-US;mso-fareast-language:EN-IN;mso-no-proof:
-    yes'><o:p></o:p></span></span></p>
-
-    <p class=MsoNormal><span style='mso-bookmark:_MailAutoSig'><span lang=EN-US
-    style='font-size:9.0pt;font-family:"Arial",sans-serif;mso-fareast-font-family:
-    "Times New Roman";mso-fareast-theme-font:minor-fareast;color:#757B80;
-    mso-ansi-language:EN-US;mso-fareast-language:EN-IN;mso-no-proof:yes'>India.</span></span><span
-    style='mso-bookmark:_MailAutoSig'><span style='mso-fareast-font-family:"Times New Roman";
-    mso-fareast-theme-font:minor-fareast;mso-fareast-language:EN-IN;mso-no-proof:
-    yes'><o:p></o:p></span></span></p>
-
     <span style='mso-bookmark:_MailAutoSig'></span>
 
     <p class=MsoNormal><o:p>&nbsp;</o:p></p>
+	
+	</div>
+
+    </body>
 "@ 
 
-    $MailBody = $MailBody -replace 'FOLDER_NAME' ,$FolderName
-    $MailBody = $MailBody -replace 'DATE_TIME' , (Get-Date -F 'dd-MMM-yyyy hh:mm' )
+    $MailBody = $MailBody -replace 'FOLDER_NAME'		 , $FolderName
+    $MailBody = $MailBody -replace 'DATE_TIME'			 , (Get-Date -F 'dd-MMM-yyyy hh:mm' )
+	$MailBody = $MailBody -replace 'MailBody_Signature'  , $MailBodySignature
 
     return $MailBody
 }
@@ -227,7 +159,7 @@ ForEach($RemoteFolderName in $RemoteFolderNames){
             $MailBody = Append-MailBody -AppendFileName "$($File.Name)" -AppendFileLastWriteTime "$(($File.LastWriteTime | Out-String ).Trim())" -AppendFileSize "$("{0:n2} KB" -f ($File.Length / 1KB))" -mailBody $MailBody
         }
         $MailBody = Sanitize-MailBody -mailBody $MailBody
-        Send-MailMessage -Body $MailBody -BodyAsHtml -From $SenderMailID -SmtpServer $SMTPServerName -To $ReveipientsList `
+        Send-MailMessage -Body $MailBody -BodyAsHtml -From $SenderMailID -SmtpServer $SMTPServerName -To $RecipientsList `
         -subject "File Status Report for folder $RemoteFolderName" # change the subject as per need...
     }
     Else{
